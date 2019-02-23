11ad814641b1af0fefa4c3430d161b480f7f72f5
Jeo D <e8gea6w5z8id@opayq.com>
2019-02-23 10:09:35 -0700

    Refactor forwardGeocode thunk

--- src/thunks/forwardGeocode.js
+++ src/thunks/forwardGeocode.js
@@ -6,14 +6,12 @@ export const forwardGeocode = (query) => {
     query = query.replace(/\s/g, '+');
     query = query.replace(/,/g, '%2C');
     try {
-      const url = `http://localhost:3001/api/v1/geocode/${query}`
+      const url = `http://localhost:3001/api/v1/forwardgeocode/${query}`;
       const geocodeData = await fetchData(url);
       const { lat: latitude, lng: longitude } = geocodeData[0].geometry;
+      const { city, state_code } = geocodeData[0].components;
+      dispatch(setCity(`${city}, ${state_code}`));
       dispatch(setCoordinates({ latitude, longitude }));
-      let city = geocodeData[0].formatted;
-      let regex = RegExp(/([A-Za-z]+(\s[A-Za-z]+)*)?,\s([A-Za-z]{2})/);
-      city = city.match(regex)[0];
-      dispatch(setCity(city));
       dispatch(setError(''));
     } catch (error) {
       dispatch(setError(error.message))
