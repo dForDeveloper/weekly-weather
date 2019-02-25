211fdd9da2c2b3dacfc5888e089df433df72fe90
Jeo D <e8gea6w5z8id@opayq.com>
2019-02-24 17:16:51 -0700

    Add CurrentWeather component

---
+++ src/components/CurrentWeather/CurrentWeather.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { LowHighRangeBar } from '../LowHighRangeBar/LowHighRangeBar';
+
+const CurrentWeather = ({ city, weather }) => {
+  const { today, week, minTemp, maxTemp } = weather;
+  const {
+    temperature,
+    feelsLike,
+    summary,
+    icon,
+    precipType,
+    precipProbability,
+    low,
+    high,
+    sunrise,
+    sunset,
+    wind,
+    humidity
+  } = today;
+
+  return (
+    <div>
+      <h1>{temperature}° and {summary} in {city}</h1>
+      <div className={icon}></div>
+      <p>Feels like: {feelsLike}°</p>
+      <p>High: {high}°</p>
+      <p>Low: {low}°</p>
+      <p>Sunrise: {sunrise}</p>
+      <p>Sunset: {sunset}</p>
+      <p>Chance of {precipType}: {precipProbability}</p>
+      <p>Wind: {wind}</p>
+      <p>Humidity: {humidity}</p>
+      {week.map((day, index) => {
+        return (
+          <LowHighRangeBar key={index} min={minTemp} max={maxTemp} {...day} />
+        );
+      })}
+    </div>
+  );
+}
+
+export default CurrentWeather;
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter, Redirect } from 'react-router-dom';
 import Search from '../Search/Search';
+import CurrentWeather from '../../components/CurrentWeather/CurrentWeather';
 import { getUserIP } from '../../thunks/getUserIP';
 import PropTypes from 'prop-types';
 
@@ -21,10 +22,8 @@ export class App extends Component {
         {!isLoading && 
           <div>
             {shouldRedirect && <Redirect to={redirectPath} />}
-            {weather.currently && !error &&
-              <h1>
-                Temperature in {city}: {parseInt(weather.currently.temperature)}
-              </h1>}
+            {weather.today && !error &&
+              <CurrentWeather city={city} weather={weather} />}
             {error && <h1>No results found</h1>}
           </div>}
         {isLoading && <h1>Loading...</h1>}
