fa3392b2defdba9bd06def7b02bf806ea9fbb55f
Jeo D <e8gea6w5z8id@opayq.com>
2019-02-24 21:37:22 -0700

    Add functionality for expanding ForecastBar, close #18

--- src/components/ForecastBar/ForecastBar.js
+++ src/components/ForecastBar/ForecastBar.js
@@ -1,23 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export const ForecastBar = ({ icon, day, min, max, high, low }) => {
-  const barStyle = {
-    width: 100 * (high - low) / (max - min) + '%',
-    marginLeft: 100 * (low - min) / (max - min) + '%'
+export class ForecastBar extends Component {
+  constructor() {
+    super();
+    this.state = { isExpanded: false };
   }
-  return (
-    <div>
-      <div className='ForecastBar'>
-        <span className={icon +' small-icon'}></span>
-        <span className='day'>{day}</span>
-        <span className='bar-container'>
-          <span className='bar' style={barStyle}>
-            <span className='low'>{low}°</span>
-            <span className='high'>{high}°</span>
+
+  handleClick = () => {
+    this.setState({ isExpanded: !this.state.isExpanded });
+  }
+
+  render() {
+    const { isExpanded } = this.state;
+    const {
+      min,
+      max,
+      day,
+      summary,
+      icon,
+      precipType,
+      precipProbability,
+      low,
+      high,
+      sunrise,
+      sunset,
+      wind,
+      humidity
+    } = this.props;
+    const barStyle = {
+      width: 100 * (high - low) / (max - min) + '%',
+      marginLeft: 100 * (low - min) / (max - min) + '%'
+    }
+    return (
+      <div>
+        <div className='ForecastBar'>
+          <span className={icon +' small-icon'}></span>
+          <span className='day'>{day}</span>
+          <span className='bar-container'>
+            <span className='bar' style={barStyle}>
+              <span className='low'>{low}°</span>
+              <span className='high'>{high}°</span>
+            </span>
           </span>
-        </span>
-        <button className='button'>+</button>
+          <button className='button' onClick={this.handleClick}>{isExpanded ? '-' : '+'}</button>
+        </div>
+        {isExpanded &&
+          <div>
+            <h3>{summary}</h3>
+            <p>Low: {low}°</p>
+            <p>High: {high}°</p>
+            <p>Sunrise: {sunrise}</p>
+            <p>Sunset: {sunset}</p>
+            <p>Chance of {precipType}: {precipProbability}</p>
+            <p>Wind: {wind}</p>
+            <p>Humidity: {humidity}</p>
+          </div>}
       </div>
-    </div>
-  );
+    );
+  }
 }
