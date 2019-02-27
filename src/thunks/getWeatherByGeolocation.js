6a3f2d7a240790e06c73c25f03eac364f91fcb25
Jeo D <e8gea6w5z8id@opayq.com>
2019-02-26 21:22:24 -0700

    Update getWeatherByGeolocation thunk

--- src/thunks/getWeatherByGeolocation.js
+++ src/thunks/getWeatherByGeolocation.js
@@ -3,12 +3,12 @@ import { reverseGeocode } from '../thunks/reverseGeocode';
 import { getWeather } from '../thunks/getWeather';
 
 export const getWeatherByGeolocation = ({ latitude, longitude }) => {
-  return (dispatch) => {
+  return async (dispatch) => {
     dispatch(toggleLoading(true));
     try{
       dispatch(setCoordinates({ latitude, longitude }));
-      dispatch(reverseGeocode({ latitude, longitude }));
-      dispatch(getWeather({ latitude, longitude }));
+      await dispatch(reverseGeocode({ latitude, longitude }));
+      await dispatch(getWeather({ latitude, longitude }));
       dispatch(setError(''));
     } catch (error) {
       dispatch(setError(error.message));
