ea8c89147d36a69606046f19137b2ec563c8b282
Jeo D <e8gea6w5z8id@opayq.com>
2019-02-26 17:14:15 -0700

    Update fetch calls to use heroku back end

--- src/thunks/forwardGeocode.js
+++ src/thunks/forwardGeocode.js
@@ -7,7 +7,7 @@ export const forwardGeocode = (userQuery) => {
     dispatch(toggleLoading(true));
     try {
       const query = userQuery.replace(/\s/g, '+').replace(/,/g, '%2C');
-      const url = `http://localhost:3001/api/v1/forwardgeocode/${query}`;
+      const url = `http://weekly-weather.herokuapp.com/api/v1/forwardgeocode/${query}`;
       const geocodeData = await fetchData(url);
       const { lat: latitude, lng: longitude } = geocodeData[0].geometry;
       const { city, state_code } = geocodeData[0].components;
--- src/thunks/reverseGeocode.js
+++ src/thunks/reverseGeocode.js
@@ -4,7 +4,7 @@ import { setCity, setError } from '../actions';
 export const reverseGeocode = ({ latitude: lat, longitude: lon }) => {
   return async (dispatch) => {
     try {
-      const url = `http://localhost:3001/api/v1/reversegeocode/${lat}/${lon}`;
+      const url = `http://weekly-weather.herokuapp.com/api/v1/reversegeocode/${lat}/${lon}`;
       const geocodeData = await fetchData(url);
       const { city, state_code } = geocodeData[0].components;
       dispatch(setCity(`${city}, ${state_code}`));
